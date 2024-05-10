3be16b621fd6e4d4b8183b26671548b639bdb488
MkaGodh <fenryrvla@yandex.ru>
2024-05-10 15:36:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <istream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    float time[10];
+    float max = 0;
+
+    for (int i = 0; i < 10; ++i) {
+        cin >> time[i];
+        if (max < time[i]) {
+            max = time[i];
+        }
+    }
+    max += 1;
+    float first = max, second = max, third = max;
+
+    for (int i = 0; i < 10; ++i) {
+        if (first > time[i]) {
+            third = second;
+            second = first;
+            first = time[i];
+        }
+        else
+            if (second > time[i]) {
+                third = second;
+                second = time[i];
+            }
+            else
+                if (third > time[i]) {
+                    third = time[i];
+                }
+    }
+    cout << "Gold:" << first << endl;
+    cout << "Silver:" << second << endl;
+    cout << "Bronze:" << third << endl;
+
+}
+
